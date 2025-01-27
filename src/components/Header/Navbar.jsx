8f80f75a91523f28a2f4b869d33d71dec2b42b8a
catmaluci <catmaluci@hotmail.com>
2025-01-27 16:23:39 +0100

    adding image logo to the navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,9 +1,10 @@
-
+import logo from "../../assets/logo.svg"
 
 function Navbar (){
 
     return (
     < nav className="navbar-container">
+        <img src={logo}alt="Soundwave logo" />
     <p>Discover</p>
     <p>Join</p>
     </nav>
