dae19b3cc0a52b16ba937a2d4cc3694aa8b2f73e
catmaluci <catmaluci@hotmail.com>
2025-01-29 21:14:05 +0100

    adding the round decoration on the background as components

---
+++ src/components/Circles/Circle3.jsx
@@ -0,0 +1,12 @@
+import './circles.css'
+
+function Circle3 () {
+
+return (
+<div className="divCircle3"></div>
+)
+
+
+}
+
+export default Circle3
--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
+import Main_Join from "../components/Mains/Main_Join/Main_join"
 
 function Join () {
 
 return (
 <>
 <Header/>
+<Main_Join/>
 <Footer/>
 </>
 )
