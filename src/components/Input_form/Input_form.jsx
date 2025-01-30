37915694444ca99269ed5798eff961292ed59849
catmaluci <catmaluci@hotmail.com>
2025-01-30 12:22:12 +0100

    form layout in page join

--- src/components/Input_form/Input_form.jsx
+++ src/components/Input_form/Input_form.jsx
@@ -1,12 +1,21 @@
-
-
-function Input_form (){
+import "./input_form.css"
+import Button from "../Button/Button"
+function Input_form() {
 
     return (
-    <>
-    
-    </>
+        <form>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" placeholder="Full Name"></input>
+           
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" placeholder="name@mail.com"></input>
+           
+            <label htmlFor="password">Password:</label>
+            <input type="text" id="password" placeholder="password"></input>
+            <Button />
+
+        </form>
     )
-    
-    }
-    export default Input_form 
+
+}
+export default Input_form 
--- src/components/Mains/Main_Join/Main_join.jsx
+++ src/components/Mains/Main_Join/Main_join.jsx
@@ -2,26 +2,20 @@ import "./main_join.css"
 
 import Circle1 from "../../Circles/Circle1"
 import Circle2 from "../../Circles/Circle2"
-import Button from "../../Button/Button"
+
+import Input_form from "../../Input_form/Input_form"
 
 function Main_Join() {
 
     return (
-        <main>
-            <h3>Join the fun.</h3>
+        <main className="formComponent">
+            <h3>Join the <span>fun.</span></h3>
+           
+            
+            <Input_form />
+           
             <Circle1 />
             <Circle2 />
-            <form>
-                {/* <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name"></input>
-                <label htmlFor="email">Email:</label>
-                <input type="text" id="email" name="email"></input>
-                <label htmlFor="password">Password:</label>
-                <input type="text" id="password" name="password"></input>
-                <br></br> */}
-                <Button />
-
-            </form>
 
         </main>
     )
